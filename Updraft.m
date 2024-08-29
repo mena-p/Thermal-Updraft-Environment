05b07b17b6eb989ed311e7b00c5f813ead9b4fdf
mena-p <119264054+mena-p@users.noreply.github.com>
2024-08-29 16:32:32 +0200

    Made it so temp and hum excesses tend to zero
    Temperature and humidity excesses are now multiplied by a ramp function when outside the updraft, reaching zero at a distance of three times the outer radius

--- Updraft.m
+++ Updraft.m
@@ -15,6 +15,7 @@ classdef Updraft
             obj.yPosition = y;
             obj.gain = gain;
             obj.timeSinceFormation = 0;
+            obj.wind_dir = 350;
             obj.coeff_uw = Updraft.load_coeff_uw();
             obj.coeff_cw = Updraft.load_coeff_cw();
 
@@ -76,7 +77,7 @@ classdef Updraft
             dist = sqrt((x - obj.xPosition)^2 + (y - obj.yPosition)^2);
         end
 
-        function angle_to_updraft = angle_to(obj, x, y)
+        function angle_from_updraft = angle_to(obj, x, y)
             % This function calculates the angle between the vector connecting
             % the updraft center to the aircraft's position and the upwind
             % direction of the updraft (wind_dir).
@@ -86,11 +87,11 @@ classdef Updraft
             % Outputs:
             % angle_to_updraft (degrees)
 
-            angle_to_x_axis = atan2d(y - obj.yPosition, x - obj.xPosition);
-            if angle_to_x_axis < 0
-                angle_to_x_axis = angle_to_x_axis + 360;
+            angle_from_x_axis = atan2d(y - obj.yPosition, x - obj.xPosition);
+            if angle_from_x_axis < 0
+                angle_from_x_axis = angle_from_x_axis + 360;
             end
-            angle_to_updraft = angle_to_x_axis - obj.wind_dir;
+            angle_from_updraft = angle_from_x_axis - obj.wind_dir;
         end
 
         function is_inside = is_inside(obj, x, y, z, zi)
@@ -107,22 +108,30 @@ classdef Updraft
         end
 
         function ptemp_diff = ptemp_diff(obj,x,y)
-            % This function calculates the potential temperature difference at the aircraft's position.
+            % This function calculates the potential temperature difference at the location x,y.
             % Outputs:
             % ptemp_diff = Potential temperature difference at the aircraft's position
 
             % Get the angle and relative distance of the aircraft to the updraft
             theta = obj.angle_to(x,y);
-            rel_dist = obj.distance_to(x,y) / obj.outer_radius(0,0);
-            rel_dist_uw = rel_dist*cos(theta/180*pi);
-            rel_dist_cw = rel_dist*sin(theta/180*pi);
+            rel_dist = obj.distance_to(x,y) ./ obj.outer_radius(0,0);
+            rel_dist_uw = rel_dist.*cos(theta*pi/180);
+            rel_dist_cw = rel_dist.*sin(theta*pi/180);
             
             % Calculate the potential temperature difference at the aircraft's position
             % by averaging the upwind and crosswind profiles based on the angle to the updraft
             ptemp_uw = obj.coeff_uw(1,1) + obj.coeff_uw(1,2)*cos(rel_dist_uw*obj.coeff_uw(1,6)) + obj.coeff_uw(1,3)*sin(rel_dist_uw*obj.coeff_uw(1,6)) + obj.coeff_uw(1,4)*cos(2*rel_dist_uw*obj.coeff_uw(1,6)) + obj.coeff_uw(1,5)*sin(2*rel_dist_uw*obj.coeff_uw(1,6));
             ptemp_cw = obj.coeff_cw(1,1) + obj.coeff_cw(1,2)*cos(rel_dist_cw*obj.coeff_cw(1,6)) + obj.coeff_cw(1,3)*sin(rel_dist_cw*obj.coeff_cw(1,6)) + obj.coeff_cw(1,4)*cos(2*rel_dist_cw*obj.coeff_cw(1,6)) + obj.coeff_cw(1,5)*sin(2*rel_dist_cw*obj.coeff_cw(1,6));
             
-            ptemp_diff = cos(theta/180*pi)^2 * ptemp_uw + sin(theta/180*pi)^2 * ptemp_cw;
+            ptemp_diff = cos(theta*pi/180)^2 * ptemp_uw + sin(theta*pi/180)^2 * ptemp_cw;
+
+            % Multiply by ramp function if the aircraft is outside the updraft, such that the potential
+            % temperature difference is multiplied by 1 at the outer radius and by zero at a distance of 3 outer radii 
+            if obj.distance_to(x,y) > obj.outer_radius(0,0) && obj.distance_to(x,y) <= 3 * obj.outer_radius(0,0)
+                ptemp_diff = ptemp_diff * (1 - (obj.distance_to(x,y) - obj.outer_radius(0,0)) / (2*obj.outer_radius(0,0)));
+            elseif obj.distance_to(x,y) > 3 * obj.outer_radius(0,0)
+                ptemp_diff = 0;
+            end
         end
 
         function humidity_diff = humidity_diff(obj,x,y)
@@ -133,14 +142,22 @@ classdef Updraft
             % Get the angle and relative distance of the aircraft to the updraft
             theta = obj.angle_to(x,y);
             rel_dist = obj.distance_to(x,y) / obj.outer_radius(0,0);
-            rel_dist_uw = rel_dist*cos(theta/180*pi);
-            rel_dist_cw = rel_dist*sin(theta/180*pi);
+            rel_dist_uw = rel_dist*cos(theta*pi/180);
+            rel_dist_cw = rel_dist*sin(theta*pi/180);
 
             % Calculate the specific humidity difference at the aircraft's position
             hum_uw = obj.coeff_uw(2,1) + obj.coeff_uw(2,2)*cos(rel_dist_uw*obj.coeff_uw(2,6)) + obj.coeff_uw(2,3)*sin(rel_dist_uw*obj.coeff_uw(2,6)) + obj.coeff_uw(2,4)*cos(2*rel_dist_uw*obj.coeff_uw(2,6)) + obj.coeff_uw(2,5)*sin(2*rel_dist_uw*obj.coeff_uw(2,6));
             hum_cw = obj.coeff_cw(2,1) + obj.coeff_cw(2,2)*cos(rel_dist_cw*obj.coeff_cw(2,6)) + obj.coeff_cw(2,3)*sin(rel_dist_cw*obj.coeff_cw(2,6)) + obj.coeff_cw(2,4)*cos(2*rel_dist_cw*obj.coeff_cw(2,6)) + obj.coeff_cw(2,5)*sin(2*rel_dist_cw*obj.coeff_cw(2,6));
 
-            humidity_diff = cos(theta/180*pi)^2 * hum_uw + sin(theta/180*pi)^2 * hum_cw;
+            humidity_diff = cos(theta*pi/180)^2 * hum_uw + sin(theta*pi/180)^2 * hum_cw;
+
+            % Multiply by ramp function if the aircraft is outside the updraft, such that the humidity
+            % difference is multiplied by 1 at the outer radius and by zero at a distance of 3 outer radii 
+            if obj.distance_to(x,y) > obj.outer_radius(0,0) && obj.distance_to(x,y) <= 3 * obj.outer_radius(0,0)
+                humidity_diff = humidity_diff * (1 - (obj.distance_to(x,y) - obj.outer_radius(0,0)) / (2*obj.outer_radius(0,0)));
+            elseif obj.distance_to(x,y) > 3 * obj.outer_radius(0,0)
+                humidity_diff = 0;
+            end
         end
     end
 
