6e5471269c22cee7ae966dbb4c2c1e0d1dc6bbbf
mena-p <119264054+mena-p@users.noreply.github.com>
2024-09-03 16:22:52 +0200

    Create detect_circles_s.m

---
+++ IGC/detect_circles_s.m
@@ -0,0 +1,32 @@
+[positions, diameters] = detect_circles_igc(flight);
+
+
+clc
+close all
+
+% Extract latitude, longitude and altitude coordinates from the trajectory
+traj_lat = flight.trajectory.lat.lat;
+traj_lon = flight.trajectory.lon.lon;
+
+% Plot trajectory
+figure
+g = geoplot(traj_lat, traj_lon, 'b-');
+title('Aircraft Trajectory')
+%legend('Aircraft Trajectory')
+
+% Add stations to the plot
+hold on
+geoscatter(stations, "lat", "lon","Marker","^","MarkerEdgeColor","g")
+hold off
+
+% Set axis limits to fit the trajectory
+geolimits([min(traj_lat) max(traj_lat)], [min(traj_lon) max(traj_lon)])
+
+% Add circles to the plot
+hold on
+for i = 1:length(positions)
+    geoplot(positions(i,1), positions(i,2), 'ro', 'MarkerSize', 10)
+    geoplot(positions(i,1), positions(i,2), 'r-', 'LineWidth', 2)
+end
+hold off
+
