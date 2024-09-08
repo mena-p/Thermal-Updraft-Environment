922c4d8e13362cee10c6eef518b272dfb1e786b7
mena-p <119264054+mena-p@users.noreply.github.com>
2024-09-08 20:07:32 +0200

    Create testing_stations.m

---
+++ IGRA-Parser/testing_stations.m
@@ -0,0 +1,30 @@
+close all
+
+% Load stations data from the stations.mat file
+load('stations.mat');
+%load('flight.mat');
+
+stations = find_nearby_stations(flight,stations,40000);
+
+% Download soundings for the selected stations
+%download_station_files(stations);
+
+% Search downloaded files for soundings on the flight date
+soundings = [];
+for i = 1:size(stations,1)
+    station = stations(i,:);
+    filename = strcat('soundings/', station.code, '-drvd.txt');
+    sounding = parse_derived_by_date(filename, flight.date);
+    soundings = [soundings; sounding];
+end
+
+% Plot trajectory and stations
+figure
+geoplot(trajectory.lat.lat, trajectory.lon.lon, 'b-')
+hold on
+geoscatter([stations.lat], [stations.lon], 'r*')
+% voronoi([stations.lat], [stations.lon]);
+hold off
+
+
+
