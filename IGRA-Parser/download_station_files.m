942d33470340985adab99302d3049506dabd8a3c
mena-p <119264054+mena-p@users.noreply.github.com>
2024-09-08 20:30:07 +0200

    Update download_station_files.m

--- IGRA-Parser/download_station_files.m
+++ IGRA-Parser/download_station_files.m
@@ -4,9 +4,9 @@ function download_station_files(stations)
     for i = 1:size(stations,1)
         station = stations(i,:);
         url = strcat('https://www.ncei.noaa.gov/data/integrated-global-radiosonde-archive/access/derived-por/', station.code, '-drvd.txt.zip');
-        filename = strcat('soundings/', station.code, '-drvd.txt.zip');
+        filename = strcat('IGRA-Parser/soundings/', station.code, '-drvd.txt.zip');
         outfilename = websave(filename,url);
-        unzip(outfilename,"soundings\");
+        unzip(outfilename,"IGRA-Parser/soundings/");
         delete(outfilename)
     end
 end
