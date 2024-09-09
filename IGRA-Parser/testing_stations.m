defe2d9c032fce64264d0e8691dd8bfe6a2ae860
mena-p <119264054+mena-p@users.noreply.github.com>
2024-09-09 17:47:09 +0200

    Update testing_stations.m

--- IGRA-Parser/testing_stations.m
+++ IGRA-Parser/testing_stations.m
@@ -2,21 +2,21 @@ close all
 
 % Load stations data from the stations.mat file
 load('stations.mat');
-%load('flight.mat');
+%load('Flights/29-Jul-2024_Schlautmann Nils.mat');
 
-stations = find_nearby_stations(flight,stations,40000);
+stations = find_nearby_stations(flight,stations,200000);
 
 % Download soundings for the selected stations
-download_station_files(stations);
+%download_station_files(stations);
 
 % Search downloaded files for soundings on the flight date
-soundings = [];
-for i = 1:size(stations,1)
-    station = stations(i,:);
-    filename = strcat('IGRA-Parser/soundings/', station.code, '-drvd.txt');
-    sounding = parse_derived_by_date(filename, flight.date);
-    soundings = [soundings; sounding];
-end
+% soundings = [];
+% for i = 1:size(stations,1)
+%     station = stations(i,:);
+%     filename = strcat('IGRA-Parser/soundings/', station.code, '-drvd.txt');
+%     found = parse_derived_by_date(filename, flight.date);
+%     soundings = [soundings, found];
+% end
 
 % Plot trajectory and stations
 figure
