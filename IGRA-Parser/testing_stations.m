5941c3be2def1d95f800aa5900249726e2f7357b
mena-p <119264054+mena-p@users.noreply.github.com>
2024-09-10 04:45:33 +0200

    Update testing_stations.m

--- IGRA-Parser/testing_stations.m
+++ IGRA-Parser/testing_stations.m
@@ -1,19 +1,20 @@
-close all
+
 
 % Load stations data from the stations.mat file
 load('stations.mat');
 %load('Flights/29-Jul-2024_Schlautmann Nils.mat');
 
-stations = find_nearby_stations(flight,stations,200000);
+active_stations = find_active_stations(flight,stations,200000);
 
+nearest_stations = find_nearest_stations(flight,active_stations);
 % Download soundings for the selected stations
-%download_station_files(stations);
+%download_station_files(nearest_stations);
 
 % Search downloaded files for soundings on the flight date
 % soundings = [];
 % for i = 1:size(stations,1)
 %     station = stations(i,:);
-%     filename = strcat('IGRA-Parser/soundings/', station.code, '-drvd.txt');
+%     filename = strcat('IGRA-Parser/soundings/', station.ID, '-drvd.txt');
 %     found = parse_derived_by_date(filename, flight.date);
 %     soundings = [soundings, found];
 % end
@@ -22,7 +23,7 @@ stations = find_nearby_stations(flight,stations,200000);
 figure
 geoplot(flight.trajectory.lat.lat, flight.trajectory.lon.lon, 'b-')
 hold on
-geoscatter([stations.lat], [stations.lon], 'r*')
+geoscatter([active_stations.lat], [active_stations.lon], 'r*')
 % voronoi([stations.lat], [stations.lon]);
 hold off
 
