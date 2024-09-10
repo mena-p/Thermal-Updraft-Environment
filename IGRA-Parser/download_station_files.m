06ebb5f52b9d1c207b65f71f2a024ca0d5e3da7c
mena-p <119264054+mena-p@users.noreply.github.com>
2024-09-10 04:44:00 +0200

    Print message when downloading

--- IGRA-Parser/download_station_files.m
+++ IGRA-Parser/download_station_files.m
@@ -3,8 +3,9 @@ function download_station_files(stations)
 %   Detailed explanation goes here
     for i = 1:size(stations,1)
         station = stations(i,:);
-        url = strcat('https://www.ncei.noaa.gov/data/integrated-global-radiosonde-archive/access/derived-por/', station.code, '-drvd.txt.zip');
-        filename = strcat('IGRA-Parser/soundings/', station.code, '-drvd.txt.zip');
+        url = strcat('https://www.ncei.noaa.gov/data/integrated-global-radiosonde-archive/access/derived-por/', station.ID, '-drvd.txt.zip');
+        filename = strcat('IGRA-Parser/soundings/', station.ID, '-drvd.txt.zip');
+        fprintf('Downloading file %s-drvd.txt.zip from IGRA archive...\n',station.ID);
         outfilename = websave(filename,url);
         unzip(outfilename,"IGRA-Parser/soundings/");
         delete(outfilename)
