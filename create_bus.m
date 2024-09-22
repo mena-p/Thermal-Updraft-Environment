6fd9334daab59c22809bc2abad5cc82dfb40bc33
mena-p <119264054+mena-p@users.noreply.github.com>
2024-09-23 01:39:07 +0200

    spelling

--- create_bus.m
+++ create_bus.m
@@ -1,27 +1,29 @@
-function sounding_busses = create_bus(sounding_array)
+function sounding_buses = create_bus(sounding_array)
 
     % Initialize array to hold the busses
-    sounding_busses = [];
+    sounding_buses = [];
     
     % For all sounding objects in the input array
     for i = 1:length(sounding_array)
         
         % Fill the bus elements with the data from the sounding object
-        new_bus.stationID = sounding_array(i).stationID;
+        %new_bus.stationID = sounding_array(i).stationID;
+        new_bus.lat = sounding_array(i).lat;
+        new_bus.lon = sounding_array(i).lon;
         new_bus.numLevels = length(sounding_array(i).derived.TEMP); % Assuming numLevels is the length of TEMP array
         new_bus.zi = sounding_array(i).mixedLayerHeight;
         new_bus.LCL = sounding_array(i).LCLheight;
         new_bus.REPGPH = sounding_array(i).derived.REPGPH;
         new_bus.PRESS = sounding_array(i).derived.PRESS;
-        new_bus.TEMP = sounding_array(i).derived.TEMP;
-        new_bus.PTEMP = sounding_array(i).derived.PTEMP;
-        new_bus.VTEMP = sounding_array(i).derived.VTEMP;
-        new_bus.VAPPRESS = sounding_array(i).derived.VAPPRESS;
-        new_bus.SATVAP = sounding_array(i).derived.SATVAP;
-        new_bus.REPRH = sounding_array(i).derived.REPRH;
+        new_bus.TEMP = sounding_array(i).derived.TEMP/10;
+        new_bus.PTEMP = sounding_array(i).derived.PTEMP/10;
+        new_bus.VTEMP = sounding_array(i).derived.VTEMP/10;
+        new_bus.VAPPRESS = sounding_array(i).derived.VAPPRESS/10;
+        new_bus.SATVAP = sounding_array(i).derived.SATVAP/10;
+        new_bus.REPRH = sounding_array(i).derived.REPRH/10;
         
         % Append the new bus to the array of busses
-        sounding_busses = [sounding_busses, new_bus];
+        sounding_buses = [sounding_buses, new_bus];
     end
     disp('Created array of sounding busses.')
 end
