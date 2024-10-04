acc1e01686fefdb9ae9803daab95a4e093e2a51a
mena-p <119264054+mena-p@users.noreply.github.com>
2024-10-04 19:36:29 +0200

    Update tune_sensor.m

--- Sensor/tune_sensor.m
+++ Sensor/tune_sensor.m
@@ -1,9 +1,18 @@
-% clear times
-% for i = 1:n
-% milli = measured_data.time(i) - start_time;
-% milli.Format = 's';
-% times(i,1) = milli;
-% end
 
-flight.trajectory.alt = timetable(times,measured_data.gps_altitude);
-temps = timetable(times,measured_data.temperature + 273.15);
+% Load data
+sensorData = importSensorData('pedro_csv.csv');
+%load('Flights/29-Jul-2024_Schlautmann Nils.mat','flight');
+load('sounding_buses.mat');
+numLevels = sounding_buses.numLevels;
+
+% convert times to duration since start
+times = sensorData.time - sensorData.time(1);
+times.Format = 's';
+
+% Create timetables
+alt = timetable(times, sensorData.gps_altitude);
+temp = timetable(times, sensorData.temperature + 273.15);
+press = timetable(times, sensorData.pressure);
+hum = timetable(times, sensorData.humidity);
+
+
