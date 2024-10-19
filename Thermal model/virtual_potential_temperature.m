7a864e9fb28b7263e1ef45309a2795396456f889
mena-p <119264054+mena-p@users.noreply.github.com>
2024-10-19 17:14:40 +0200

    Update virtual_potential_temperature.m

--- Thermal model/virtual_potential_temperature.m
+++ Thermal model/virtual_potential_temperature.m
@@ -15,7 +15,8 @@ function vpt = virtual_potential_temperature(T, RH, p)
     %
     %   Outputs:
     %       vpt - virtual potential temperature in kelvin
-        theta = t*(100000/p)^0.286; % potential temperature
+
+        theta = T*(100000/p)^0.286; % potential temperature
         esat = 6.1094 * exp(17.625 * (T - 273.15)/(T - 273.15 + 243.04)); % saturated vapor pressure hPa
         e = RH * esat / 100; % vapor pressure hPa
         r = 0.622 * e/(p-e); % mixing ratio, unitless
