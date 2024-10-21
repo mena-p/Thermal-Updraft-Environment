5896c2224e1e8f83c7b18b5e7125d89db30a5fd9
mena-p <119264054+mena-p@users.noreply.github.com>
2024-10-22 00:06:21 +0200

    Fixed bugs

--- Charts/updatePosition.m
+++ Charts/updatePosition.m
@@ -1,13 +1,13 @@
-function updatePosition(y,time,sigNum)
-    % Get GUI handle
+function updatePosition(y,time,sigNum) 
+% Get GUI handle
     position = findobj(allchild(0),'Tag', 'position');
     % Get current data
     currentLat = position.LatitudeData;
     currentLon = position.LongitudeData;
     % Append new data
-    latData = [currentLat y(1)];
-    lonData = [currentLon y(2)];
+    latData = [currentLat y(1,1)];
+    lonData = [currentLon y(1,2)];
     % Update plot
     set(position,'LatitudeData',latData,'LongitudeData',lonData);
-    drawnow;
+    drawnow limitrate;
 end
--- Charts/updateVelocity.m
+++ Charts/updateVelocity.m
@@ -2,8 +2,8 @@ function updateVelocity(y,time,sigNum)
     % Get GUI handle
     airspeed = findobj(allchild(0),'Tag', 'airspeed');
     climb = findobj(allchild(0),'Tag', 'climb');
-    vel = sqrt(y*y') * 1.943844; % knots
-    vert = y(3) * 196.85039370078738; % ft/min
+    vel = sqrt(y(1,:)*y(1,:)') * 1.943844; % knots
+    vert = y(1,3) * 196.85039370078738; % ft/min
     set(airspeed,'Airspeed',vel);
     set(climb,'ClimbRate',vert);
     drawnow;
