d341f6b48d772f839c60f8c295b748dc5f6a1469
mena-p <119264054+mena-p@users.noreply.github.com>
2024-10-30 18:47:17 +0100

    Implemented a station cache
    The station cache stores the line where the soundings for each day can be found in the station file. This allows soundings to be found quicker.

---
+++ IGRA-Parser/create_station_cache.m
@@ -0,0 +1,47 @@
+function create_station_cache(stationID)
+% Creates a cache for a given station. 
+%
+% This function creates a cache for a specified station by extracting
+% sounding dates and line numbers from the derived parameter file for that
+% station. The cache is saved as a .mat file in the Cache folder.
+
+    filename = fullfile('IGRA-Parser', 'Cache', strcat(stationID, '-cache.mat'));
+    stationFilename = fullfile('IGRA-Parser', 'Stations', strcat(stationID, '-drvd.txt'));
+    
+    if ~isfile(stationFilename)
+        error(strcat('There is no derived parameter file for station',...
+            ' ', stationID, '. Download one first'))
+        return
+    end
+
+    fprintf('Station %s not yet cached. Caching...\n', stationID);
+
+    % Create a new cache
+    cache = table('Size',[0 2],'VariableNames', {'date', 'line'}, 'VariableTypes', {'datetime', 'uint32'});
+    % Set timezone to UTC
+    cache.date.TimeZone = 'UTC';
+
+    % Initialize line counter
+    lineCounter = uint32(0);
+
+    % Open corresponding station file and extract sounding dates and line numbers
+    fileid = fopen(stationFilename);
+    while(~feof(fileid))
+        line = fgetl(fileid);
+        lineCounter = lineCounter + 1;
+
+        if strcmp(line(1),'#') % Header line
+            % Date of sounding
+            date = datetime(line(14:23),'InputFormat',...
+            'yyyy MM dd','TimeZone','UTC');
+
+            % If the date is not already in the cache, add it
+            if ~any(cache.date == date)
+                cache = [cache; {date, lineCounter}];
+            end
+        end
+    end
+    fclose(fileid);
+    save(filename, 'cache');
+    fprintf('Finished caching %s\n\n',stationID);
+end
--- IGRA-Parser/download_station_files.m
+++ IGRA-Parser/download_station_files.m
@@ -1,15 +1,27 @@
 function download_station_files(stations)
-%DOWNLOAD_STATION_FILE Summary of this function goes here
-%   Detailed explanation goes here
+% Downloads and processes station files from IGRA archive
+%   This function takes a list of station structures, downloads the corresponding
+%   data files from the IGRA archive, unzips them, and updates the cache for each station.
+
     for i = 1:size(stations,1)
         station = stations(i,:);
         url = strcat('https://www.ncei.noaa.gov/data/integrated-global-radiosonde-archive/access/derived-por/', station.ID, '-drvd.txt.zip');
-        filename = strcat('IGRA-Parser/soundings/', station.ID, '-drvd.txt.zip');
+        filename = strcat('IGRA-Parser/Stations/', station.ID, '-drvd.txt.zip');
         fprintf('Downloading file %s-drvd.txt.zip from IGRA archive...\n',station.ID);
         outfilename = websave(filename,url);
-        unzip(outfilename,"IGRA-Parser/soundings/");
+        unzip(outfilename,"IGRA-Parser/Stations/");
         delete(outfilename)
+
+        fprintf('Downloaded file for station %s\n',station.ID);
+
+        % Update or create cache for stations
+        cacheFilename = strcat('IGRA-Parser/Cache/',station.ID, '-cache.mat');
+        if ~isfile(cacheFilename)
+            create_station_cache(station.ID);
+        else
+            update_station_cache(station.ID);
+        end
     end
-    fprintf('Finished downloading station files. Ready to find soundings\n');
+    fprintf('Finished downloading station files and updating cache.\nReady to find soundings.\n\n');
 end
 
