4d2c72d013a58b8e801b4de42c238cb74462548b
mena-p <119264054+mena-p@users.noreply.github.com>
2024-10-31 11:53:04 +0100

    Guarantee that at least one active station is found
    Function now checks the cache to see if the station is active on the day of the flight, not only the year, and expands the search radius if nothing is found. Automatically downloads new station files if they need to be updated.

--- IGRA-Parser/find_active_stations.m
+++ IGRA-Parser/find_active_stations.m
@@ -1,4 +1,4 @@
-function stations = find_active_stations(flight, stations, max_dist)
+function active_stations = find_active_stations(flight, stations, max_dist)
     % FIND_NEARBY_STATIONS - Filter stations based on flight date and position range
     %
     %   Syntax:
@@ -10,11 +10,12 @@ function stations = find_active_stations(flight, stations, max_dist)
     %       - max_dist: Maximum distance (in meters) allowed between a station and the flight trajectory.
     %
     %   Output Argument:
-    %       - stations: Filtered table of stations that meet the criteria.
+    %       - active_stations: Filtered table of stations that meet the criteria.
 
     % Get flight date
     date = flight.date; % datetime object
-    date = year(date);
+    date.TimeZone = 'UTC';
+    flightYear = year(date);
 
     % Get flight position bounding box
     trajectory = flight.trajectory;
@@ -26,23 +27,46 @@ function stations = find_active_stations(flight, stations, max_dist)
     mean_lon = mean(trajectory.lon.lon);
     
     % Only keep stations working during the flight year
-    mask = [stations.firstYear] <= datenum(date) & [stations.lastYear] ...
-        >= datenum(date);
+    mask = [stations.firstYear] <= datenum(flightYear) & [stations.lastYear] ...
+        >= datenum(flightYear);
     stations = stations(mask, :);
     
-    % Filter out stations further away than max_dist
-    dist1 = distance([stations.lat], [stations.lon], max_lat, max_lon,...
-        wgs84Ellipsoid);
-    dist2 = distance([stations.lat], [stations.lon], max_lat, min_lon,...
-        wgs84Ellipsoid);
-    dist3 = distance([stations.lat], [stations.lon], min_lat, max_lon,...
-        wgs84Ellipsoid);
-    dist4 = distance([stations.lat], [stations.lon], min_lat, min_lon,...
-        wgs84Ellipsoid);
-    dist5 = distance([stations.lat], [stations.lon], mean_lat, mean_lon,...
-        wgs84Ellipsoid);
+    % Initialize output
+    active_stations = [];
+    while(isempty(active_stations))
+        % Filter out stations further away than max_dist
+        dist1 = distance([stations.lat], [stations.lon], max_lat, max_lon,...
+            wgs84Ellipsoid);
+        dist2 = distance([stations.lat], [stations.lon], max_lat, min_lon,...
+            wgs84Ellipsoid);
+        dist3 = distance([stations.lat], [stations.lon], min_lat, max_lon,...
+            wgs84Ellipsoid);
+        dist4 = distance([stations.lat], [stations.lon], min_lat, min_lon,...
+            wgs84Ellipsoid);
+        dist5 = distance([stations.lat], [stations.lon], mean_lat, mean_lon,...
+            wgs84Ellipsoid);
 
-    mask = dist1 < max_dist | dist2 < max_dist | dist3 < max_dist | ...
-        dist4 < max_dist | dist5 < max_dist;
-    stations = stations(mask,:);
+        mask = dist1 < max_dist | dist2 < max_dist | dist3 < max_dist | ...
+            dist4 < max_dist | dist5 < max_dist;
+        stations = stations(mask,:);
+
+        % Find out which stations are not up to date and update their files
+        old_stations = stations(stations.lastUpdate < date, :);
+        download_station_files(old_stations);
+
+        % Find out which stations are active on the day of the flight with the cache
+        active_stations = [];
+        for i = 1:size(stations,1)
+            station = stations(i,:);
+            filename = fullfile('IGRA-Parser', 'Cache', strcat(station.ID, '-cache.mat'));
+            if isfile(filename)
+                load(filename, 'cache');
+                if any(cache.date == date)
+                    active_stations = [active_stations; station];
+                end
+            end
+        end
+        % If no stations are active, increase the distance
+        max_dist = max_dist + 200000;
+    end
 end
