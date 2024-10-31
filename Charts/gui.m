569bef30a955759bf5b8f6e0c80c50c5ebc62eba
mena-p <119264054+mena-p@users.noreply.github.com>
2024-10-31 17:20:47 +0100

    Expanded GUI

--- Charts/gui.m
+++ Charts/gui.m
@@ -12,18 +12,17 @@ function gui()
     %% Create UI components
     fig = uifigure("Position",[100 100 800 450],"Name",'Soarsense','Tag','GUI');
     tabs = uitabgroup("Parent",fig,"Units","normalized","Position",[0 0 1 1]);
+    
+    % Tab 1
     tab1 = uitab("Parent",tabs,"Title",	"Setup");
     grid = uigridlayout(tab1,[1 2]);
     grid.ColumnWidth = {'1x','2x'};
     subgrid = uigridlayout(grid,[2 1]);
-    tab2 = uitab("Parent",tabs,"Title","Simulation");
-    grid2 = uigridlayout(tab2,[1 1]);
+    buttongrid = uigridlayout(subgrid,[5 2]);
+    buttongrid.Layout.Row = 2;
+    buttongrid.Layout.Column = 1;
     
-    % Plots Simulation Tab
-    ax2 = axes(grid2);
-    data_plot = plot(0,0,'-r','Parent',ax2,"Tag","dataPlot");
-    set(data_plot,'XData',[],"YData",[]);
-
+    % Plots in tab 1
     % Plots Setup Tab
     ax = geoaxes(grid);
     traj_plot = geoplot(0,0,'-b','Parent',ax);
@@ -36,24 +35,44 @@ function gui()
     set(active_station_plot,'XData',[],"YData",[]);
     set(nearest_station_plot,'XData',[],"YData",[]);
 
-    % Table
+    % Sounding table
     sounding_tbl = uitable(subgrid);
+    sounding_tbl.Layout.Row = 1;
+    sounding_tbl.Layout.Column = 1;
     sounding_tbl.SelectionType = "row";
     sounding_tbl.Multiselect = "on";
     sounding_tbl.Data = table('Size',[1 2],'VariableTypes',["string",...
         "datetime"],'VariableNames',["Station ID","Sounding time"]);
     sounding_tbl.SelectionChangedFcn = @(src,event) select_soundings(src,event);
 
-    % Buttons
-    bg = uibuttongroup(subgrid);
-    b1 = uibutton(bg,"Text","Load flight","Position",[10 180 100 22]);
-    b2 = uibutton(bg,"Text","Detect thermals","Position",[10 155 100 22]);
-    b3 = uibutton(bg,"Text","Add thermal","Position",[10 130 100 22]);
-    b4 = uibutton(bg,"Text","Remove thermal","Position",[10 105 100 22]);
-    b5 = uibutton(bg,"Text","Remove all","Position",[10 80 100 22]);
-    b6 = uibutton(bg,"Text","Download data","Position",[10 55 100 22]);
-    b7 = uibutton(bg,"Text","Find soundings","Position",[10 30 100 22]);
-    b8 = uibutton(bg,"Text","Send to model","Position",[10 5 100 22]);
+   % Buttons
+   b1 = uibutton(buttongrid,"Text","Load flight");
+   b1.Layout.Row = 1;
+   b1.Layout.Column = 1;
+   b9 = uibutton(buttongrid,"Text","Create Flight");
+   b9.Layout.Row = 1;
+   b9.Layout.Column = 2;
+   b2 = uibutton(buttongrid,"Text","Detect thermals");
+   b2.Layout.Row = 2;
+   b2.Layout.Column = 1;
+   b3 = uibutton(buttongrid,"Text","Add thermal");
+   b3.Layout.Row = 2;
+   b3.Layout.Column = 2;
+   b4 = uibutton(buttongrid,"Text","Remove thermal");
+   b4.Layout.Row = 3;
+   b4.Layout.Column = 1;
+   b5 = uibutton(buttongrid,"Text","Remove all");
+   b5.Layout.Row = 3;
+   b5.Layout.Column = 2;
+   b6 = uibutton(buttongrid,"Text","Download data");
+   b6.Layout.Row = 4;
+   b6.Layout.Column = 1;
+   b7 = uibutton(buttongrid,"Text","Find soundings");
+   b7.Layout.Row = 4;
+   b7.Layout.Column = 2;
+   b8 = uibutton(buttongrid,"Text","Send to model");
+   b8.Layout.Row = 5;
+   b8.Layout.Column = 1;
     
     % Configure buttons
     b1.ButtonPushedFcn = @(src,event) load_flight();
@@ -64,17 +83,47 @@ function gui()
     b6.ButtonPushedFcn = @(src,event) download_data();
     b7.ButtonPushedFcn = @(src,event) find_soundings();
     b8.ButtonPushedFcn = @(src,event) send_to_model();
+
+    % Tab 2
+    tab2 = uitab("Parent",tabs,"Title","Simulation");
+    simulationGrid = uigridlayout(tab2,[1 2]);
+    simulationSubgridLeft = uigridlayout(simulationGrid,[2 1]);
+    simulationSubgridRight = uigridlayout(simulationGrid,[2 1]);
+    simulationSubgridLeft.RowHeight = {'9x','1x'};
+    simulationSubgridRight.RowHeight = {'3x','1x'};
     
-  
-    % % Load data
-    % try
-    %     traj_plot = plot_flight();
-    % catch
-    % end
-    % try
-    %     updraft_plot = plot_updrafts();
-    % catch
-    % end
+    % Plots in tab 2
+    % Instruments plot
+    instrimentSubgrid = uigridlayout(simulationSubgridRight,[1 3]);
+    instrimentSubgrid.Layout.Row = 2;
+    instrimentSubgrid.Layout.Column = 1;
+    airspeed = uiaeroairspeed(instrimentSubgrid,"Tag","airspeed","Limits",[0 150]);
+    altimeter = uiaeroaltimeter(instrimentSubgrid,"Tag","altimeter");
+    climb = uiaeroclimb(instrimentSubgrid,"Tag","climb");
+
+    % Aircaft plot
+    ax3 = geoaxes(simulationSubgridLeft);
+    ax3.Layout.Row = 1;
+    ax3.Layout.Column = 1;
+    position = geoplot(0,0,'-b','Parent',ax3,"Tag","position");
+    set(position,'LatitudeData',[],'LongitudeData',[]);
+
+    % Thermals plot
+    updraft_plot_sim = geoscatter(0,0,'r',"Marker",'o',"Parent",ax3);
+    set(updraft_plot_sim,'XData',[],"YData",[]);
+
+    % Arrow plot
+    ax2 = polaraxes("Parent",simulationSubgridRight);
+    ax2.Layout.Row = 1;
+    ax2.Layout.Column = 1;
+    arrow_plot = compassplot(0,1,'Parent',ax2,"Tag","arrowPlot");
+    title(ax2,"Body-axis VPT gradient direction")   
+    set(ax2,"ThetaZeroLocation",'top',"ThetaDir",'clockwise')
+
+    % Simulation controls
+    simControl = uisimcontrols(simulationSubgridLeft);
+    simControl.Layout.Row = 2;
+    simControl.Layout.Column = 1;
     
     
     %% Button callback functions
@@ -209,80 +258,41 @@ function gui()
         plot_updrafts();
     end
 
-    %% Plotting functions
-    % Plot flight trajectory
-    function plot_flight()
-        flight = evalin("base",'flight');
-        traj_lat = flight.trajectory.lat.lat;
-        traj_lon = flight.trajectory.lon.lon;
-        set(traj_plot,'XData',traj_lat,'YData',traj_lon);
-        geolimits(ax,[0.99*min(traj_lat) 1.01*max(traj_lat)], [0.99*min(traj_lon) 1.01*max(traj_lon)])
-    end
-
-    % Plot updrafts
-    function plot_updrafts()
-        % Check if updraft_locations is loaded
-        if ~evalin("base",'exist(''updraft_locations'', ''var'')')
-            updraft_locations = zeros(0, 0);
-            assignin("base", 'updraft_locations', updraft_locations)
-        end
-
-        updraft_locations = evalin("base",'updraft_locations');
-        if ~isempty(updraft_locations)
-            set(updraft_plot,'XData',updraft_locations(:,1),"YData",updraft_locations(:,2));
-        else
-            set(updraft_plot,'XData',[],"YData",[]);
-        end
-    end
-    % Plot stations
-    function show_stations()
+    % Find soundings
+    function find_soundings()
+    
         if ~evalin("base",'exist(''flight'', ''var'')')
             disp("Please load a flight first.")
             return
         end
-
         flight = evalin("base",'flight');
-
-        active = find_active_stations(flight,stations,100000);
-        nearest = find_nearest_stations(flight,active);
-
-        set(active_station_plot,'XData',active.lat,"YData",active.lon);
-        set(nearest_station_plot,'XData',nearest.lat,"YData",nearest.lon);
-    end
-
-    function find_soundings()
     
-            if ~evalin("base",'exist(''flight'', ''var'')')
-                disp("Please load a flight first.")
-                return
-            end
-            flight = evalin("base",'flight');
-        
-            found_soundings = [];
-            for i = 1:size(nearest,1)
-                station = nearest(i,:);
-                found = parse_derived_by_date(station.ID, flight.date);
-                found_soundings = [found_soundings, found];
-            end
-            %found_soundings = filter_soundings(found_soundings); 
-            soundings = struct2table(found_soundings,"AsArray",true);
-            t = soundings;
-            vars = ["stationID","time"];
-            newNames = ["Station ID","Sounding time"];
-            t = renamevars(t,vars,newNames);
-            t = t(:,newNames);
+        found_soundings = [];
+        for i = 1:size(nearest,1)
+            station = nearest(i,:);
+            found = parse_derived_by_date(station.ID, flight.date);
+            found_soundings = [found_soundings, found];
+        end
+        %found_soundings = filter_soundings(found_soundings); 
+        soundings = struct2table(found_soundings,"AsArray",true);
+        t = soundings;
+        vars = ["stationID","time"];
+        newNames = ["Station ID","Sounding time"];
+        t = renamevars(t,vars,newNames);
+        t = t(:,newNames);
 
-            % Configure table
-            sounding_tbl.Data = t;
-            sounding_tbl.SelectionType = "row";
-            sounding_tbl.Multiselect = "on";
+        % Configure table
+        sounding_tbl.Data = t;
+        sounding_tbl.SelectionType = "row";
+        sounding_tbl.Multiselect = "on";
     end
-    %% Download data
+    
+    % Download data
     function download_data(src,event)
         download_station_files(nearest);
     end
 
-    %% Prepare everything for simulink
+    % Send to model
     function send_to_model()
         % Check if everything is ready
         
@@ -321,7 +331,7 @@ function gui()
         selected_soundings_structs = table2struct(selected_soundings);
         for i = 1:size(selected_soundings_structs,1)
             % Extract only the necessary data from the sounding
-        	reduced_soundings(i) = extract_sounding_data(selected_soundings_structs(i));
+            reduced_soundings(i) = extract_sounding_data(selected_soundings_structs(i));
         end
 
         % Get lowest number of levels among all soundings
@@ -362,17 +372,49 @@ function gui()
         
     end
 
-% Show station names when hovering over the markers wohooooooooooooo
-% gcm_obj = datacursormode(fig);
-% set(gcm_obj,'UpdateFcn',@data_cursor_updatefcn)
-% % Callback function to display station names
-% function output_txt = data_cursor_updatefcn(~, event_obj)
-%     % Display the position of the data cursor
-%     pos = event_obj.Position;
-%     idx = event_obj.DataIndex;
-%         output_txt = {['Station: ' stations.ID{idx}]};
-% end
+    %% Plotting functions
+    % Plot flight trajectory
+    function plot_flight()
+        flight = evalin("base",'flight');
+        traj_lat = flight.trajectory.lat.lat;
+        traj_lon = flight.trajectory.lon.lon;
+        set(traj_plot,'XData',traj_lat,'YData',traj_lon);
+        geolimits(ax,[0.99*min(traj_lat) 1.01*max(traj_lat)], [0.99*min(traj_lon) 1.01*max(traj_lon)])
+    end
 
+    % Plot updrafts
+    function plot_updrafts()
+        % Check if updraft_locations is loaded
+        if ~evalin("base",'exist(''updraft_locations'', ''var'')')
+            updraft_locations = zeros(0, 0);
+            assignin("base", 'updraft_locations', updraft_locations)
+        end
+
+        updraft_locations = evalin("base",'updraft_locations');
+        if ~isempty(updraft_locations)
+            set(updraft_plot,'XData',updraft_locations(:,1),"YData",updraft_locations(:,2));
+        else
+            set(updraft_plot,'XData',[],"YData",[]);
+        end
+    end
+
+    % Plot stations
+    function show_stations()
+        if ~evalin("base",'exist(''flight'', ''var'')')
+            disp("Please load a flight first.")
+            return
+        end
+
+        flight = evalin("base",'flight');
+
+        active = find_active_stations(flight,stations,100000);
+        nearest = find_nearest_stations(flight,active);
+
+        set(active_station_plot,'XData',active.lat,"YData",active.lon);
+        set(nearest_station_plot,'XData',nearest.lat,"YData",nearest.lon);
+    end
+    
+    % Other UI functions
     % Select soundings from table
     function select_soundings(src,event)
         rows = event.Selection;
@@ -384,7 +426,18 @@ function gui()
         end
         
     end
+    
 
+% Show station names when hovering over the markers wohooooooooooooo
+% gcm_obj = datacursormode(fig);
+% set(gcm_obj,'UpdateFcn',@data_cursor_updatefcn)
+% % Callback function to display station names
+% function output_txt = data_cursor_updatefcn(~, event_obj)
+%     % Display the position of the data cursor
+%     pos = event_obj.Position;
+%     idx = event_obj.DataIndex;
+%         output_txt = {['Station: ' stations.ID{idx}]};
+% end
 
 end
 
