4330315742a05a1e2100094579aac306faac5999
mena-p <119264054+mena-p@users.noreply.github.com>
2024-10-31 11:50:27 +0100

    Added 'lastUpdated' field to stations.mat
    Keeps track of when the last download took place. Station files are only downloaded by 'find_active_stations' if they are out of date.

--- IGRA-Parser/download_station_files.m
+++ IGRA-Parser/download_station_files.m
@@ -1,10 +1,10 @@
-function download_station_files(stations)
+function download_station_files(stationList)
 % Downloads and processes station files from IGRA archive
 %   This function takes a list of station structures, downloads the corresponding
 %   data files from the IGRA archive, unzips them, and updates the cache for each station.
 
-    for i = 1:size(stations,1)
-        station = stations(i,:);
+    for i = 1:size(stationList,1)
+        station = stationList(i,:);
         url = strcat('https://www.ncei.noaa.gov/data/integrated-global-radiosonde-archive/access/derived-por/', station.ID, '-drvd.txt.zip');
         filename = strcat('IGRA-Parser/Stations/', station.ID, '-drvd.txt.zip');
         fprintf('Downloading file %s-drvd.txt.zip from IGRA archive...\n',station.ID);
@@ -21,7 +21,12 @@ function download_station_files(stations)
         else
             update_station_cache(station.ID);
         end
+        % Update lastUpdate in station.mat file
+        load("IGRA-Parser/stations.mat");
+        stations.lastUpdate(stations.ID == station.ID) = datetime('now','TimeZone','UTC');
+        save("IGRA-Parser\stations.mat","stations");
+        fprintf('Registered update in stations.mat\n\n');
     end
-    fprintf('Finished downloading station files and updating cache.\nReady to find soundings.\n\n');
+    fprintf('Finished downloading station files and updating cache.\n\n');
 end
 
