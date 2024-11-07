89b96b4b45d46f73303fcd7bef87e0e2bf259fd9
mena-p <119264054+mena-p@users.noreply.github.com>
2024-11-07 18:34:01 +0100

    changed to test humidity

--- Thermal model/test_thermal_model.m
+++ Thermal model/test_thermal_model.m
@@ -11,15 +11,15 @@ num_iterations = length(lat);
 
 % Loop to test the thermal_model function
 for i = 1:num_iterations
-    [T, q, p] = updraft_model(lat(i), lon(i), alt(i), [pi/4,0,0], sounding_buses, updrafts);
+    [T, q, p,RH] = updraft_model(lat(i), lon(i), alt(i), [pi/4,0,0], sounding_buses, updrafts);
     fprintf('Iteration %d: alt = %f, 1 = %f,2 = %f,3 = %f\n', i, alt(i), RH(1),RH(2),RH(3));
     %fprintf('Iteration %d: alt = %f, w1 = %f, w2 = %f, sum = %f\n', i, alt(i), w(1), w(2), w(1)+w(2))
 end
 
-function [T, q, p] = updraft_model(lat, lon, alt, euler_angles, sounding_buses, updrafts)
+function [T, q, p,RH] = updraft_model(lat, lon, alt, euler_angles, sounding_buses, updrafts)
     
     if size(updrafts,1)
-        [T, q, p] = thermal_model(lat, lon, alt, euler_angles, updrafts, sounding_buses);
+        [T, q, p,RH] = thermal_model(lat, lon, alt, euler_angles, updrafts, sounding_buses);
     else
         % Suppress Simulink coder error
         T = 0;
