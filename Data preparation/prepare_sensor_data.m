107d0214060261a0826b89fb1006584169e14158
mena-p <119264054+mena-p@users.noreply.github.com>
2024-11-18 19:27:26 +0100

    renamed humidity timetable

--- Data preparation/prepare_sensor_data.m
+++ Data preparation/prepare_sensor_data.m
@@ -2,11 +2,11 @@
 % This script is used to prepare sensor data for the simulink model
 % when you want to run it using data collected in flight. The data should
 % be provided in the same format as the file 'pedro_csv.csv' created by 
-% Leo.
+% Leo. You should still do the setup process as usual on the GUI, and then 
+% run this script.
 
 % Load data
 sensorData = importSensorData('Raw data/pedro_csv.csv');
-numLevels = sounding_buses.numLevels;
 
 % convert times to duration since start
 times = sensorData.time - sensorData.time(1);
@@ -16,4 +16,4 @@ times.Format = 's';
 alt = timetable(times, sensorData.gps_altitude);
 temp = timetable(times, sensorData.temperature + 273.15);
 press = timetable(times, sensorData.pressure);
-hum = timetable(times, sensorData.humidity);
+RH = timetable(times, sensorData.humidity);
