cfe0d1f5d9f6871035d5c0f3e4a352a6b213e984
mena-p <119264054+mena-p@users.noreply.github.com>
2024-11-20 13:37:49 +0100

    made script self-contained

--- Validation/validationScript.m
+++ Validation/validationScript.m
@@ -1,10 +1,20 @@
+% This script was used to validate the full simulation environment. If you
+% want to verify the validation, run every section and the sensor tuner
+% model (follow the instructions in the comments). Or you can skip
+% the first and second sections by uncommenting the load command on line 48
+% and running from there.
+
 close all
 clear
 % Load data
 sensorData = importSensorData('Raw data/pedro_csv.csv');
 load("sounding_buses.mat","sounding_buses");
-sounding = sounding_buses(1);
-load('Flights/29-Jul-2024_Schlautmann Nils.mat')
+sounding_buses = sounding_buses(1);
+load("updrafts_full_validation.mat","updrafts")
+load("Processed data/sounding_bus.mat",'sounding')
+addpath 'C:\Users\Pedro\Documents\Faculdade\Bachelorarbeit\Thermal-Updraft-Model\Flights'
+load('29-Jul-2024_Schlautmann Nils.mat')
+rmpath 'C:\Users\Pedro\Documents\Faculdade\Bachelorarbeit\Thermal-Updraft-Model\Flights'
 numLevels = sounding_buses.numLevels;
 
 % convert times to duration since start
@@ -28,15 +38,16 @@ vel_squared = timetable(times, sensorData.velocity.^2);
 % Convert times to double
 times = seconds(times);
 
-% Setup the simulation and run the sensor tuner model with the
+%% Run the SensorTuner.slx model
+% Run the sensor tuner model with the
 % full simulation environment model now. Make sure to set the simulation
-% step size to 1s as the sensor model constants have been adjusted to this
-% rate.
+% step size to 1s as the cockpit sensor model constants have been adjusted 
+% for this rate.
 
-%% Extract simulation results (with thermals)
-load('Validation/simulation_out_sensor_model.mat')
+%% Extract simulation results
+%load('Validation/simulation_out_sensor_model.mat')
 
-% Extrac model results
+% Extract model results
 temps = out.yout{1}.Values.Data;
 RHs = out.yout{2}.Values.Data;
 ps = out.yout{3}.Values.Data;
@@ -54,93 +65,16 @@ thermalp = squeeze(ps(1,2,:));
 sensorp = squeeze(ps(1,5,:));
 alts = simalts;
 
-%% Extract simulation results (without thermals)
-load('Validation/simulation_out_sensor_model_no_thermal.mat')
-
-% Extrac model results
-temps = out_no_thermal.yout{1}.Values.Data;
-RHs = out_no_thermal.yout{2}.Values.Data;
-ps = out_no_thermal.yout{3}.Values.Data;
-simalts = out_no_thermal.yout{4}.Values.Data;
-simtimes = out_no_thermal.tout;
-
-realT = squeeze(temps(1,1,:));
-thermalT2 = squeeze(temps(1,2,:));
-sensorT2 = squeeze(temps(1,5,:));
-realRH = squeeze(RHs(1,1,:));
-thermalRH2 = squeeze(RHs(1,2,:));
-sensorRH2 = squeeze(RHs(1,5,:));
-realp = squeeze(ps(1,1,:));
-thermalp = squeeze(ps(1,2,:));
-sensorp = squeeze(ps(1,5,:));
-alts = simalts;
-
-difference = thermalT - thermalT2;
-%% Make a mask for the thermals
-dists = distance(latitude,longitude,sounding.lat,sounding.lon,wgs84Ellipsoid('m'));
-%dists = dists(1:50:609101); % Sample dists in 1s intervals
-diffs = diff(alts);
-mask = diffs>0 & dists(1:end-1) < 39400 & dists(1:end-1) > 38700;% 
-
-% Plot the trajectory and station location
-close all
-figure
-%geoplot(latitude,longitude)
-title('Flight Segment')
-
-%geoplot(sounding.lat,sounding.lon,'b^',"LineStyle","none")
-%hold on
-geoplot(latitude(mask), longitude(mask))
-legend('Full','Station','Section')
 %% Calculate correlation coeffs on entire flight
 corr_temp = corrcoef(realT,sensorT);
 corr_hum = corrcoef(realRH,sensorRH);
 corr_p = corrcoef(realp,sensorp);
 
-%% Calculate correlations coeffs on thermals
-corr_temp = corrcoef(realT(mask),sensorT(mask));
-corr_hum = corrcoef(realRH(mask),sensorRH(mask));
-corr_p = corrcoef(realp(mask),sensorp(mask));
-
-corr_temp2 = corrcoef(realT(mask),sensorT2(mask));
-corr_hum2 = corrcoef(realRH(mask),sensorRH2(mask));
-corr_p2 = corrcoef(realp(mask),sensorp(mask));
 %% Calculate correlation coeffs excluding surges
 corr_temp2 = corrcoef(realT(alts<2004),sensorT(alts<2004));
 corr_hum2 = corrcoef(realRH(alts<2004),sensorRH(alts<2004));
 corr_p2 = corrcoef(realp(alts<2004),sensorp(alts<2004));
 
-%% Plot values inside the thermal only
-zi = 2004;
-
-close all
-f1 = figure('Position', [10 10 900 300]);
-plot(simtimes(mask), realT(mask),"Color","b")
-hold on
-plot(simtimes(mask), thermalT(mask),'Color','#0072BD')
-plot(simtimes(mask),sensorT(mask))
-plot(simtimes(mask), thermalT2(mask),'Color','#0072BD')
-plot(simtimes(mask),sensorT2(mask))
-legend('Flight Test', 'Thermal Model','Sensor Model',"Location","southeast")
-xlabel('Flight Time [s]')
-ylabel("Temperature [K]")
-xlim([0 12183])
-%xline(idx, '--','HandleVisibility','off')
-grid on
-%saveas(f1,"Images/Validation/comparison-temp",'png')
-
-f2 = figure('Position', [10 10 900 300]);
-plot(simtimes(mask),realRH(mask),"Color","b")
-hold on
-plot(simtimes(mask),thermalRH(mask),'Color','#0072BD')
-plot(simtimes(mask),sensorRH(mask))
-legend('Flight Test', 'Thermal Model',"Sensor Model","location","southeast")
-xlabel('Flight Time [s]')
-ylabel("Relative Humidity [%]")
-xlim([0 12183])
-%xline(idx, '--','HandleVisibility','off')
-grid on
-%saveas(f2,"Images/Validation/comparison-hum",'png')
 %% Plot real, thermal model, and sensor model values
 zi = 2004;
 
@@ -195,5 +129,3 @@ xlim([0 12183])
 %hold on
 %xline(idx, '--','HandleVisibility','off')
 %saveas(gcf,"Images/Sensor/comparison-alt",'png')
-
-
